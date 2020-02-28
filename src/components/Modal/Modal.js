32d54b2166a5323939130e13e00c90b8e7329344
Federico Marino <fede.m.fede@gmail.com>
2020-02-28 12:55:14 -0300

    wip
    git

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,16 +3,19 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faTimes} from '@fortawesome/free-solid-svg-icons';
 import './Modal.scss';
 
-const Modal = ({isOpen, closeModal, children}) => {
+const Modal = ({isOpen, closeModal, children, height, width}) => {
   
     if(!isOpen) return null;
     return (
-      <section className="modal">
-        <button onClick={closeModal} type="button" className="close-icon">
-          <FontAwesomeIcon icon={faTimes} />
-        </button>
-        {children}
-      </section>
+      <div>
+        <div className="overlay"></div>
+        <section className="modal" style={{height, width}}>
+          <button onClick={closeModal} type="button" className="close-icon">
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+          {children}
+        </section>
+      </div>
   );
 };
 
