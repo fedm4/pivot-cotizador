4ec3807dafd986ec63644b63fdbc5c2d83ae3e9a
Federico Marino <fede.m.fede@gmail.com>
2020-02-29 08:27:05 -0300

    using .env

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+const config = {
+  gappsClientId: process.env.REACT_APP_GAPPS_CLIENT_ID,
+  gappsClientSecret: process.env.REACT_APP_GAPPS_CLIENT_SECRET,
+  gappsApiKey: process.env.REACT_APP_GAPPS_API_KEY,
+  mongoConnection: ``
+};
+
+export default config;
